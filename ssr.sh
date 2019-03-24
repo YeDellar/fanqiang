e528bea81adc850d06d831746c9153ae2bfe4224
YeDellar <48879016+YeDellar@users.noreply.github.com>
2019-03-24 17:33:31 +0800

    Update ssr.sh

--- ssr.sh
+++ ssr.sh
@@ -6,8 +6,8 @@ export PATH
 #	System Required: CentOS 6+/Debian 6+/Ubuntu 14.04+
 #	Description: Install the ShadowsocksR server
 #	Version: 2.0.38
-#	Author: Toyo
-#	Blog: https://doub.io/ss-jc42/
+#	Author: YeDellar
+#	Blog: https://user.qzone.qq.com/735873423
 #=================================================
 
 sh_ver="2.0.38"
